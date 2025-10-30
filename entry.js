b032ab0d3f69d3232999ca00ac4b80b85cee26e0
johnsomwu <johnsomwu@tencent.com>
2025-10-30 14:54:43 +0800

    feat: 增加输入后日志

--- entry.js
+++ entry.js
@@ -81,8 +81,10 @@ const max = "--instances 1";                 // 按CPU核心数最大化进程
 const memmory = `--max-memory-restart "1G"`; // 内存超1GB自动重启
 const cron = `--cron "0 4 * * *"`; // 每日UTC 04:00定时重启
 const timezone = `--env TZ=UTC`; // 强制使用UTC时区
+const logger = `--output /root/.pm2/logs/pdf2img-output.log`;
+const errorLogger = `--error /root/.pm2/logs/pdf2img-error.log`;
 
-const stableStr = `${name} ${max} ${memmory} ${cron} ${timezone}`;
+const stableStr = `${name} ${max} ${memmory} ${cron} ${timezone} ${logger} ${errorLogger}`;
 const localCommand = `${baseCommand} && ${extraEnvStr} pm2 start app.js ${stableStr}`;
 const dockerCommand = `${baseCommand} && ${extraEnvStr} pm2-runtime start app.js ${stableStr}`;
 
